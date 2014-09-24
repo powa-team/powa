9d40418a049be27a742f64cf5c170b3835288dee
Julien Rouhaud <julien.rouhaud@free.fr>
2014-09-24 14:32:53 +0200

    ui: Move db selector to user_menu

--- ui/lib/Helpers/Menu.pm
+++ ui/lib/Helpers/Menu.pm
@@ -14,32 +14,14 @@ sub register {
         user_menu => sub {
             my $self = shift;
             my $html = '';
+            my $dbh;
+            my $sql;
+            my @dbs;
 
             if ( $self->session('user_username') ) {
                 $self->stash(
                     menu_username => $self->session('user_username') );
 
-                $html = $self->render(
-                    template => 'helpers/user_menu',
-                    partial  => 1 );
-            }
-
-            return b($html);
-        } );
-
-    $app->helper(
-        main_menu => sub {
-            my $self = shift;
-            my $html;
-            my $dbh;
-            my $sql;
-            my @dbs;
-
-            my $level = "guest";
-            $level = "user"  if ( $self->session('user_username') );
-            $level = "admin" if ( $self->session('user_admin') );
-
-            if ( $level ne "guest" ) {
                 $dbh = $self->database();
                 $sql = $dbh->prepare("SELECT DISTINCT dbname FROM public.powa_statements ORDER BY dbname");
                 $sql->execute();
@@ -49,14 +31,15 @@ sub register {
                 }
                 $sql->finish();
                 $dbh->disconnect();
-            }
 
-            $self->stash(
-                user_level => $level,
-                dbs        => \@dbs
-            );
-            $html =
-                $self->render( template => 'helpers/main_menu', partial => 1 );
+                $self->stash(
+                    dbs        => \@dbs
+                );
+
+                $html = $self->render(
+                    template => 'helpers/user_menu',
+                    partial  => 1 );
+            }
 
             return b($html);
         } );
