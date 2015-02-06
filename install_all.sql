b67f0b14a718e513e15a05fac63bd3f042aa8ee9
Ronan Dunklau <ronan@dunklau.fr>
2015-02-06 13:24:48 +0100

    Add sample install scripts

---
+++ install_all.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE powa;
+\c powa
+CREATE EXTENSION btree_gist;
+CREATE EXTENSION pg_stat_statements;
+CREATE EXTENSION pg_qualstats;
+CREATE EXTENSION pg_stat_kcache;
+CREATE EXTENSION powa;
