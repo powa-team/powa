91dfafc4ba2843152687d7de0f07b7c8bd0da9b1
Ronan Dunklau <ronan@dunklau.fr>
2015-11-06 17:22:35 +0100

    Version bumps

--- docs/conf.py
+++ docs/conf.py
@@ -59,12 +59,12 @@ copyright = '2015, Dalibo'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '3.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0'
+release = '3.0.0'
 powa_archivist_release = release
-powa_web_release = '2.0.11'
-pg_qualstats_release = '0.0.6'
+powa_web_release = '3.0.0'
+pg_qualstats_release = '0.0.7'
 pg_stat_kcache_release = 'REL2_0_2'
 
 
