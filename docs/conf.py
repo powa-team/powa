99dc283606e1729e871e2911cd08095ddaedaea3
Julien Rouhaud <julien.rouhaud@free.fr>
2019-04-02 00:02:55 +0200

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'PoWA'
-copyright = '2015-2017, Dalibo - 2018, The PoWA-team'
+copyright = '2015-2017, Dalibo - 2018-2019, The PoWA-team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
