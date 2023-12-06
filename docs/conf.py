a50672ae7e24d4ebbeb1a51247b1c44041b56a33
Julien Rouhaud <julien.rouhaud@free.fr>
2023-12-06 14:25:21 +0100

    Alaways use the sphinx_rtd_theme theme.

--- docs/conf.py
+++ docs/conf.py
@@ -118,13 +118,9 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-# on_rtd is whether we are on readthedocs.io, this line of code grabbed from docs.readthedocs.io
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.io uses their theme by default, so no need to specify it
 
